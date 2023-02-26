459b0c20516ba859e7afea5bd54bdb5b91a8118d
ilyanabatchikov <inabatchikov@dpkapp.ru>
2023-02-26 21:28:08 +0300

    remove route

--- api/main.py
+++ api/main.py
@@ -35,30 +35,3 @@ def get_weather(city: str):
         raise HTTPException(
             404, crud_error_message(f"No weather found for city: {city}")
         )
-
-
-@app.get("/tasks/{task_id}")
-def task_status(task_id: str):
-    """
-    Get task status.
-    PENDING (waiting for execution or unknown task id)
-    STARTED (task has been started)
-    SUCCESS (task executed successfully)
-    FAILURE (task execution resulted in exception)
-    RETRY (task is being retried)
-    REVOKED (task has been revoked)
-    """
-    task = AsyncResult(task_id)
-    state = task.state
-
-    if state == "FAILURE":
-        error = str(task.result)
-        response = {
-            "state": state,
-            "error": error,
-        }
-    else:
-        response = {
-            "state": state,
-        }
-    return response
